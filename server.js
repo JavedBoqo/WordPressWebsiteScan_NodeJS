438552889e796de19bb1911d1ea2485a2153bba8
javedboqo <javedboqo@gmail.com>
2021-05-20 19:37:13 +0500

    Make processes as asynch/await

--- server.js
+++ server.js
@@ -9,28 +9,16 @@ const port = process.env.PORT || 3000;
 
 app.post('/', async (req, res) => {
     const body = req.body;
-    console.log(body.url);
-    const finalRep = await require('./lib/app').wpcheck(
+    // console.log(body.url);
+    const info = await require('./lib/app').wpcheck(
         require('minimist')(
             body.url,
             require('./config/minimist.json')
         )
-    )//*/
-
-    /*require('./lib/app').wpcheck(
-        require('minimist')(
-            body.url,
-            require('./config/minimist.json')
-        )
-    ).then((res) => {
-        console.log("final res",res);
-    })//*/
-
-    //   res.send('Hello World!')
-    // console.log('FINAL', finalRep)
-    res.json(finalRep);
+    );
+    res.json(info);
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Wordpress Scanning app listening at http://localhost:${port}`)
 })
