87c20b830b06a72e05deff0c15c5833ccbe40f30
javedboqo <javedboqo@gmail.com>
2021-05-21 12:32:02 +0500

    change port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.post('/', async (req, res) => {
     const body = req.body;
